f03c0597c849af82ab5487a0d70062b73dba6a68
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-12-07 00:17:02 +0300

    Update lazy_loading_models.g.dart

--- lib/lazy_loading_models.g.dart
+++ lib/lazy_loading_models.g.dart
@@ -77,14 +77,14 @@ NewLazyLoadingModel<T> _$NewLazyLoadingModelFromJson<T>(
       currentPage: json['current_page'] as int,
       data: (json['data'] as List<dynamic>).map(fromJsonT).toList(),
       firstPageUrl: json['first_page_url'] as String,
-      from: json['from'] as int,
+      from: json['from'] as int?,
       lastPage: json['last_page'] as int,
       lastPageUrl: json['last_page_url'] as String,
       nextPageUrl: json['next_page_url'] as String?,
       path: json['path'] as String,
       perPage: json['per_page'] as int,
       prevPageUrl: json['prev_page_url'] as String?,
-      to: json['to'] as int,
+      to: json['to'] as int?,
       total: json['total'] as int,
     );
 
